d6bfb86bd43ed022e011d9ea58163ce06135768e
vulcan6 <Shadeius2>
2020-12-04 17:21:38 -0600

    Havent graded but its completed I think

--- 12.7.py
+++ 12.7.py
@@ -4,15 +4,27 @@
 
 def get_age():
     age = int(input())
-    # TODO: Raise excpetion for invalid ages
+    # Age required to be between 18 and 75 otherwise bring an error
+    if age < 18 or age > 75:
+    raise ValueError("Invalid age")
     return age
+# Should be fine. WIP
 
-# TODO: Complete fat_burning_heart_rate() function
 def fat_burning_heart_rate(age):
-
+    # Adult Fat Burning Heart Rate = (70% * 220) - Age
+    heart_rate = 220 - age
+    heart_rate *= 0.7
     return heart_rate
 
 if __name__ == "__main__":
-    # TODO: Modify to call get_age() and fat_burning_heart_rate()
-    #       and handle the exception
+    try:
+
+    age = get_age()
+    rate = fat_burning_heart_rate(age)
+    print("Fat burning rate for a ", age, " year-old:")
+    print(rate, "bpm")
+    except ValueError as excpt:
+    print(excpt)
+    print("Could not calculate heart rate")
+    
     age = get_age()
