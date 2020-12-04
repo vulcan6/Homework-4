949449bec586de5729012dd6c9d6ff96ca363e74
vulcan6 <Shadeius2>
2020-12-04 16:51:57 -0600

    Intialized Struggle

--- 12.7.py
+++ 12.7.py
@@ -1,3 +1,18 @@
 # Erick Jimenez
 # PSID 1463639
 # Zylab 12.7
+
+def get_age():
+    age = int(input())
+    # TODO: Raise excpetion for invalid ages
+    return age
+
+# TODO: Complete fat_burning_heart_rate() function
+def fat_burning_heart_rate(age):
+
+    return heart_rate
+
+if __name__ == "__main__":
+    # TODO: Modify to call get_age() and fat_burning_heart_rate()
+    #       and handle the exception
+    age = get_age()
