e94bf38d41070692781c285617000bfd65d87a79
vulcan6 <Shadeius2>
2020-12-04 18:08:53 -0600

    A commitment to trying at least.

--- 12.9.py
+++ 12.9.py
@@ -5,12 +5,12 @@
  # Split input into 2 parts: name and age
 parts = input().split()
 name = parts[0]
+    # while names and ages end with -1. Hoping I understood that right
 while name != '-1':
-    # FIXME: The following line will throw ValueError exception.
-    #        Insert try/except blocks to catch the exception.
-    age = int(parts[1]) + 1
+    try:
+        age = int(parts[1]) + 1
+    except Exception as ex:
+        age = 0
     print('{} {}'.format(name, age))
-
-    # Get next line
     parts = input().split()
     name = parts[0]
