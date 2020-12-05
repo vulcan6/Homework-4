3ccb8995d482b11460fb4f7c72af45978a7ef2ba
vulcan6 <Shadeius2>
2020-12-04 18:12:31 -0600

    Starting

--- 14.11.py
+++ 14.11.py
@@ -1,3 +1,13 @@
 # Erick Jimenez
 # PSID 1463639
 # Zylab 14.11
+
+# TODO: Write a selection_sort_descend_trace() function that
+#       sorts the numbers list into descending order
+def selection_sort_descend_trace(numbers):
+
+
+if __name__ == "__main__":
+    # TODO: Read in a list of integers into numbers, then call
+    #       selection_sort_descend_trace() to sort the numbers
+    numbers = []
