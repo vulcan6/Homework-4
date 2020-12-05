ead56eb94bf3c3a74699b037ce0e0b820a414c23
vulcan6 <Shadeius2>
2020-12-04 20:04:19 -0600

    Got it but no points I'm missing a space

--- 14.11.py
+++ 14.11.py
@@ -4,10 +4,18 @@
 
 # TODO: Write a selection_sort_descend_trace() function that
 #       sorts the numbers list into descending order
-def selection_sort_descend_trace(numbers):
+def selection_sort_descend_trace(nmbrlst):
+    for i in range(len(nmbrlst) - 1):
+        index = i
+        for j in range(i + 1, len(nmbrlst)):
+            if nmbrlst[j] > nmbrlst[index]:
+                index = j
+        nmbrlst[i], nmbrlst[index] = nmbrlst[index], nmbrlst[i]
+        print(' '.join([str(x) for x in nmbrlst]))
+
+    return nmbrlst
 
 
-if __name__ == "__main__":
-    # TODO: Read in a list of integers into numbers, then call
-    #       selection_sort_descend_trace() to sort the numbers
-    numbers = []
+if __name__ == '__main__':
+    nmbr = [int(x) for x in input().split()]
+    selection_sort_descend_trace(nmbr)
